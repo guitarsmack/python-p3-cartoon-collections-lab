f9dc06ef196b51fc29ac0b40116618c4a04b6770
Michael Alan Stowe <mastowe94@gmail.com>
2024-01-22 20:29:59 -0600

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
+def roll_call_dwarves(names):
+    for name in names:
+        print(f"{str(names.index(name) + 1 )}. {name.capitalize()}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(calls):
+    return [ call.capitalize() + '!' for call in calls ]
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(calls):
+    while True:
+        for call in calls:
+            if len(call) > 4:
+                return True
+        return False
+
     pass
 
-def find_the_cheese():
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheeses:
+            return snack
+    return None
     pass
